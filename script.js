3251f474c88d1cef0a4ef6d392936c2a335cbe7e
SHARAN MURLI <45029621+sharanmurli@users.noreply.github.com>
2022-02-02 15:40:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function moveToSection(x){
+
+    switch(x){
+        
+
+        
+
+        case 'about_section_up_arrow':
+            window.scroll({
+                top:0,
+                left:0,
+                behavior:"smooth"
+
+            })
+            break;
+        
+            
+       
+
+        
+
+
+    }
+
+}
